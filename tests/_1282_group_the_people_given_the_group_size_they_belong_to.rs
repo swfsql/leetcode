5d057125cfb6f5d8ca8af85d92fedb2e9904986c
Thi <swfsql@gmail.com.br>
2020-04-14 02:22:55 -0300

    fix ordering in a test

--- tests/_1282_group_the_people_given_the_group_size_they_belong_to.rs
+++ tests/_1282_group_the_people_given_the_group_size_they_belong_to.rs
@@ -2,8 +2,7 @@ use leetcode::_1282_group_the_people_given_the_group_size_they_belong_to::*;
 
 #[test]
 fn _1282_group_the_people_given_the_group_size_they_belong_to_0() {
-    assert_eq!(
-        Solution::group_the_people(vec![3, 3, 3, 3, 3, 1, 3]),
-        vec![vec![5], vec![0, 1, 2], vec![3, 4, 6]]
-    );
+    let mut res = Solution::group_the_people(vec![3, 3, 3, 3, 3, 1, 3]);
+    res.sort_by(|a, b| a.len().partial_cmp(&b.len()).unwrap());
+    assert_eq!(res, vec![vec![5], vec![0, 1, 2], vec![3, 4, 6]]);
 }
