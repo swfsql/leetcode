3f4629a7869f7fba54ebb7e03c0584267ba3d8cb
swfsql <swfsql@users.noreply.github.com>
2020-09-03 21:43:34 +0000

    deploy: 39624d04e6f8d62532da0c23b40ce6fd157aac84

--- doc/leetcode/_0044_wildcard_matching/sidebar-items.js
+++ doc/leetcode/_0044_wildcard_matching/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["_is_match","Idiomatic entry point."]],"struct":[["Solution",""],["Walker",""]]});
+initSidebarItems({"fn":[["_is_match","Idiomatic entry point."],["xx",""]],"struct":[["Solution",""],["Walker",""]]});
