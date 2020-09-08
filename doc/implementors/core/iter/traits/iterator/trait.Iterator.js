60c0041901ffda7c1f9c6d0c99b249209a82206e
swfsql <swfsql@users.noreply.github.com>
2020-09-08 21:17:00 +0000

    deploy: 9581dac169de52ba9011208b6370ed957a8dc862

--- doc/implementors/core/iter/traits/iterator/trait.Iterator.js
+++ doc/implementors/core/iter/traits/iterator/trait.Iterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["leetcode"] = [{"text":"impl&lt;'s&gt; Iterator for Walker&lt;'s&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'s&gt; Iterator for Walker&lt;'s&gt;","synthetic":false,"types":[]}];
+implementors["leetcode"] = [{"text":"impl&lt;'s&gt; Iterator for Walker&lt;'s&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'s&gt; Iterator for Walker&lt;'s&gt;","synthetic":false,"types":[]},{"text":"impl Iterator for Fib","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
